9e47f3f9375d107e3c813aed613295fb16060344
anigerla <regina.zaripova@gmail.com>
2022-10-28 17:33:52 -0400

    Additional content

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header.js";
 import Hero from "./components/Hero.js";
+import katie from "./images/image 12.png";
+import bride from "./images/wedding-photography 1.png";
+import bike from "./images/mountain-bike 1.png";
+import Card from "./components/Card.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <div className="card_container">
+        <Card 
+          img={katie}
+          score="5.0"
+          review_count={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price="136"
+        />
+        <Card 
+          img={bride}
+          score="5.0"
+          review_count={30}
+          country="USA"
+          title="Learn Wedding Photography"
+          price="215"
+        />
+        <Card 
+          img={bike}
+          score="4.8"
+          review_count={2}
+          country="USA"
+          title="Group Mountain Biking"
+          price="50"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Card.css';
+import star from '../images/Star 1.png';
+
+export default function Card(props) {
+    return (
+        <div className="card">
+           <img className="card--image" src={props.img} alt="" />
+           <div className="card--stats">
+                <img className="card--star" src={star} alt="" />
+                <span>{props.score}</span>
+                <span className="gray">({props.review_count}) • </span>
+                <span className="gray">{props.country}</span>
+           </div>
+           <p>{props.title}</p>
+           <p><span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    )
+}
