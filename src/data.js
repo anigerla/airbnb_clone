0eedded0bd6f6c245dc9cd8d39187e2752b95eb0
anigerla <regina.zaripova@gmail.com>
2022-10-30 09:52:23 -0400

    Image fix

--- src/data.js
+++ src/data.js
@@ -15,7 +15,7 @@ export default [
         id: 2, 
         title: "Learn Wedding Photography",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pellentesque, lacus et bibendum faucibus, nisi ipsum bibendum mi, sit amet facilisis arcu nulla quis lorem. Aenean vitae mauris a massa egestas luctus vel nec ante. Ut risus sapien, ultricies vel libero ullamcorper, commodo dictum purus. Nullam mi ex, pharetra aliquet molestie ut, varius at dolor. Donec sodales porta eleifend. Aliquam malesuada, mauris ut sodales molestie, nisi eros fringilla leo, id tempus mi mauris eget dolor. Sed nec lacus eu velit hendrerit rutrum. Nullam dictum mollis nisl sit amet facilisis. Suspendisse odio urna, tristique at vestibulum sed, ultricies sit amet mauris. Vestibulum sit amet velit at augue ornare bibendum. Sed eget augue quis mauris ultrices laoreet. Nunc sodales velit et lacus aliquam, eu gravida urna feugiat. Praesent congue vulputate nibh quis egestas. Suspendisse quis elementum urna.",
-        img: "wedding-photography.png",
+        img: "wedding-photography 1.png",
         stats: {
             score: 5.0,
             review_count: 30
